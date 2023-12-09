7dd3a7ff5f811a6addbc55c295ecb10c45ab9106
jill64 <intents.turrets0h@icloud.com>
2023-12-09 18:56:41 +0900

    fix: invalid imports

--- demo/src/hooks.server.ts
+++ demo/src/hooks.server.ts
@@ -1,10 +1,8 @@
 import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
-import { ogpAttach, onRender } from '@jill64/svelte-suite'
-import { sequence } from '@sveltejs/kit/hooks'
 
 const { onHandle, onError } = serverInit(
   'https://54e9f3318af09e412ba479733f4d689d@o4505814639312896.ingest.sentry.io/4506320964943872'
 )
 
-export const handle = onHandle(sequence(onRender(), ogpAttach))
+export const handle = onHandle()
 export const handleError = onError()
