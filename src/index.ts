8da821f42ad16938d63bf2900bfc7ca82b82e0b8
jill64 <intents.turrets0h@icloud.com>
2024-02-18 22:41:12 +0900

    feat: tw plugin export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 export { default as gridAuto } from '@jill64/tailwind-grid-auto'
 export { default as reactions } from '@jill64/tailwind-reactions'
+
 export type { Config as PostcssConfig } from 'postcss-load-config'
 export type { Config as TailwindConfig } from 'tailwindcss'
+export { default as plugin } from 'tailwindcss/plugin.js'
+
 export { postcssConfig } from './postcssConfig.js'
 export { tailwindConfig } from './tailwindConfig.js'
